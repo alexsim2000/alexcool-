7ca337c373ad6d549ff85a353339f02abc5f5249
alexsim2000 <165932668+alexsim2000@users.noreply.github.com>
2024-04-17 19:36:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pygame import *
 from random import *
 windov = display.set_mode((700, 500))
-display.set_caption('Игра про тарелки и ракету')
+display.set_caption('')
 backgroud = transform.scale(image.load('Remove-bg.ai_1712763450316.png'), (700, 500))
 game = True
 clock = time.Clock()
@@ -14,38 +14,84 @@ score = 0
 lost = 0
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, color):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
+class Player(sprite.Sprite):
+    def __init__(self, color, speed , x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((50, 150))
         self.image.fill(color)
         self.rect = self.image.get_rect()
+        self.speed = speed
+        self.rect.x = x
+        self.rect.y = y
      
 
     def update(self):
-    keys= pygame.key.get_pressed()
-    if keys[K_s]
+        keys= key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -=self.speed
+        if keys[K_s] and self.rect.y < 450:
+            self.rect.y +=self.speed
 
 
-   
+    def update2(self):
+        keys= key.get_pressed()
+        if keys[K_UP]:
+            self.rect.y -=self.speed
+        if keys[K_DOWN]:
+            self.rect.y +=self.speed
 
+
+class Ball(sprite.Sprite):
+    def __init__(self, color, speed_x ,speed_y, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((50, 50))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        ball.rect.x += ball.speed_x
+        ball.rect.y += ball.speed_y
+
+        if ball.rect.y > 500-50 or ball.rect.y < 0:
+            ball.speed_y *= -1
+
+
+ball = Ball((225, 225, 225), 2, 2, 300, 320)   
+player1= Player((225, 225, 225), 20, 20, 20)
+player2= Player((225, 225, 225), 20, 600, 20)
 
 
 finish = False
 while game:
     if finish != True:
         
-        text3 = font4. render('ТЫ ВЫИГРАЛ ;)', 1, (234, 255, 0))
-        text4 = font5. render('ТЫ ПРОИГРАЛ :(', 1, (234, 255, 0))
+
+
+
+
+        player2.update2()
+        player1.update()
+        windov.blit(backgroud, (0, 0))
+        windov.blit(ball.image, (ball.rect.x, ball.rect.y))
+        windov.blit(player1.image, (player1.rect.x, player1.rect.y))
+        windov.blit(player2.image, (player2.rect.x, player2.rect.y))
+        
+        ball.update()
+
+
+        if sprite.collide_rect(player2, ball) or sprite.collide_rect(player1, ball):
+            ball.speed_x*=-1
+            
+  
      
       
         for e in event.get():  
             if e.type ==QUIT:
                 game = False
-            if e.type == KEYDOWN:
-                if e.key == K_SPACE:
-                    raceta.fier()
-
 
     display.update()
     clock.tick(50)
